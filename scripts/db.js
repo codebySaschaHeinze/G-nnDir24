af09315358aa149b4de8440102bd44c331204336
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-07-17 18:49:22 +0200

    new start

---
+++ scripts/db.js
@@ -0,0 +1,44 @@
+let products = [
+  {
+    name: "Stewie’s Evil Delight",
+    price: 11.49,
+    description:
+      "Pikante Peperoni, scharfe Jalapeños und ein Hauch Ziegenkäse.",
+  },
+  {
+    name: "Quackmire’s Special",
+    price: 10.99,
+    description: "Würzig-scharfe Salami trifft auf doppelt Käse.",
+  },
+  {
+    name: "Lois’ Veggie Delight",
+    price: 9.99,
+    description:
+      "Frische Paprika, Champignons, Zwiebeln und Brokkoli auf Tomatensauce.",
+  },
+  {
+    name: "Clevelands BBQ Dream",
+    price: 11.49,
+    description: "Zarte Hähnchenstücke, rauchige BBQ-Soße und rote Zwiebeln.",
+  },
+  {
+    name: "Joe’s Power Pizza",
+    price: 12.49,
+    description: "Extra viel Mozzarella, Bacon, Peperoni und Jalapeños.",
+  },
+  {
+    name: "Chris’ Cheesy Beast",
+    price: 10.99,
+    description: "Doppelt Käse, extra Teig, fette Salamischeiben.",
+  },
+  {
+    name: "Brian’s Truffle Temptation",
+    price: 12.99,
+    description: "Edle Trüffelcreme, Spinat, Mozzarella und schwarze Oliven.",
+  },
+  {
+    name: "Meg’s Four Cheese Classic",
+    price: 9.49,
+    description: "Mozzarella, Gouda, Gorgonzola und Parmesan.",
+  },
+];
