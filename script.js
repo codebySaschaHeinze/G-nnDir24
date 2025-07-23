db799b0b845a7c6bc13c676f061c45f83a3207f7
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-07-23 16:21:21 +0200

    style: made it a bit prettier: script: wrote the doc. got the jsdoc.json and made a gitignore.

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 function init() {
-  loadCartFromStorage();
+  loadCartFromLocalStorage();
   renderProducts();
   renderCartAndRenderTotalPriceAndIfCartIsEmpty();
 }
 
 let cart = [];
 
-// productIndex enthält alle Produkte vom Array
+/**
+ * productIndex contains all products from the products-array
+ * and gives it to addProductsTemplate
+ */
 function renderProducts() {
   let productsRef = document.getElementById("all_products");
   productsRef.innerHTML = "";
-
   for (let productIndex = 0; productIndex < products.length; productIndex++) {
     productsRef.innerHTML += addProductsTemplate(productIndex);
   }
-  renderTotalPrice();
 }
 
-// cartIndex enthält alle Produkte im Warenkorb
+/** cartIndex contains all products from the products-array
+ *  and gives it to addProductsToCartTemplate
+ */
 function renderCart() {
   let cartRef = document.getElementById("cart_products_container");
   cartRef.innerHTML = "";
@@ -27,7 +30,8 @@ function renderCart() {
   }
 }
 
-// prüft ob Warenkorb leer ist und gibt/entfernt d-none
+/** checks if cart is empty
+ */
 function ifCartIsEmpty() {
   let emptyCart = document.getElementById("empty_cart");
   if (cart.length == 0) {
@@ -37,44 +41,52 @@ function ifCartIsEmpty() {
   }
 }
 
-// ausgewähltes Prdukt zum Warenkorb hinzufügen
+/** add selected product to Cart
+ *  checks if selectetd product is already in cart
+ */
 function addToCart(productIndex) {
   for (let cartIndex = 0; cartIndex < cart.length; cartIndex++) {
     if (cart[cartIndex].name === products[productIndex].name) {
       cart[cartIndex].amount++;
-      saveCartToStorage();
+      saveCartToLocalStorage();
       renderCartAndRenderTotalPriceAndIfCartIsEmpty();
       updateOverlayIfVisible();
       return;
     }
   }
   cart.push({ name: products[productIndex].name, amount: 1, price: products[productIndex].price });
-  saveCartToStorage();
+  saveCartToLocalStorage();
   renderCartAndRenderTotalPriceAndIfCartIsEmpty();
   updateOverlayIfVisible();
 }
 
-// Warenkorb - price und amount erhöhen
+/** increase price and amount from product in cartIndex
+ */
 function plusCartProduct(cartIndex) {
   cart[cartIndex].amount++;
-  saveCartToStorage();
+  saveCartToLocalStorage();
   renderCartAndRenderTotalPriceAndIfCartIsEmpty();
   renderCartOverlayContent();
 }
 
-// Warenkorb - Produkt verringern (sollte amount 1 oder kleiner sein - raussplicen)
+/** decrease price and amount from product in cartIndex
+ *  decrease both IF amount is higher than 1
+ *  ELSE - splice product out of the cart.array
+ */
 function minusCartProduct(cartIndex) {
   if (cart[cartIndex].amount <= 1) {
     cart.splice(cartIndex, 1);
   } else {
     cart[cartIndex].amount--;
   }
-  saveCartToStorage();
+  saveCartToLocalStorage();
   renderCartAndRenderTotalPriceAndIfCartIsEmpty();
   renderCartOverlayContent();
 }
 
-// alle product.price´s addieren (und * product.amount).
+/** calculate the price of a product in the cart-array
+ *  price * amount
+ */
 function calculateAllCartProducts(cart) {
   let totalPrices = 0;
   for (let totalPriceIndex = 0; totalPriceIndex < cart.length; totalPriceIndex++) {
@@ -82,7 +94,10 @@ function calculateAllCartProducts(cart) {
   }
   return totalPrices;
 }
-// Gesamtpreis vom Warenkorb im Button anzeigen
+/** show total price on the total-price-button
+ * there is a button on the desktop-version
+ * and one on the mobile-version (mediaQuery)
+ */
 function renderTotalPrice() {
   let mobileRef = document.getElementById("total_price_container");
   let desktopRef = document.getElementById("total_price_container_desktop");
@@ -93,7 +108,10 @@ function renderTotalPrice() {
     desktopRef.innerHTML = renderTotalPriceButtonDesktopTemplate();
   }
 }
-// rendert das Warenkorb Overlay
+
+/** shows the cart overlay in the mobile-version (mediaQuery)
+ *  removes the total-price-button (if there)
+ */
 function renderCartOverlay() {
   const overlayRef = document.getElementById("cart_overlay_container");
   overlayRef.classList.remove("d-none");
@@ -105,20 +123,27 @@ function renderCartOverlay() {
   renderCartOverlayContent();
 }
 
-// bei onclick auf Gesamtpreisbutton rendert der Warenkorb im Overlay
+/** showing the cart overlay at onclick="renderCartOverlay()"
+ * (total-price-button in the mobile-version)
+ *
+ */
 function renderCartOverlayContent() {
   const overlayRef = document.getElementById("cart_overlay_container");
   overlayRef.innerHTML = cartOverlayTemplate();
 }
 
-/*  */
+/** update the cart overlay if cart_overlay_container is visible
+ */
 function updateOverlayIfVisible() {
   let overlayRef = document.getElementById("cart_overlay_container");
   if (!overlayRef.classList.contains("d-none")) {
     renderCartOverlayContent();
   }
 }
-// schließt bei onclick das Warenkorb Overlay
+
+/** close the overlay at onclick="closeCartOverlay()"
+ * (close-overlay-button in the mobile-version)
+ */
 function closeCartOverlay() {
   let overlayRef = document.getElementById("cart_overlay_container");
   overlayRef.classList.add("d-none");
@@ -127,39 +152,49 @@ function closeCartOverlay() {
     mobileButton.classList.remove("d-none");
   }
 }
-// Bestellung abgeben
+
+/** shows showOrderOverlay if there are products in the cart
+ *  if there are no products in the cart, nothing happens
+ */
 function orderAccepted() {
   if (cart.length === 0) return;
   closeCartOverlay();
   showOrderOverlay();
   cart = [];
-  saveCartToStorage();
+  saveCartToLocalStorage();
   renderCartAndRenderTotalPriceAndIfCartIsEmpty();
 }
 
+/** close the order overlay at onclick="closeOrderOverlay()"
+ * (close-overlay-button in the mobile-version)
+ */
 function closeOrderOverlay() {
   const overlayRef = document.getElementById("order_overlay_container");
   overlayRef.classList.add("d-none");
   overlayRef.innerHTML = "";
 }
 
+/** shows the order overlay if the order is accepted
+ * and gives it to the orderOverlayTemplate
+ */
 function showOrderOverlay() {
   const orderOverlayRef = document.getElementById("order_overlay_container");
   orderOverlayRef.innerHTML = orderOverlayTemplate();
   orderOverlayRef.classList.remove("d-none");
 }
 
-function saveCartToStorage() {
+/** saves the current condition in the local storage  */
+function saveCartToLocalStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
-
-function loadCartFromStorage() {
-  const savedCart = localStorage.getItem("cart");
+/** gives the saved informations to the functions */
+function loadCartFromLocalStorage() {
+  let savedCart = localStorage.getItem("cart");
   if (savedCart) {
     cart = JSON.parse(savedCart);
   }
 }
-
+/** helper function to reduce code blocks */
 function renderCartAndRenderTotalPriceAndIfCartIsEmpty() {
   renderCart();
   renderTotalPrice();
